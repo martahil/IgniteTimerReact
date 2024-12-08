d4f4e5d9aa15e01b0dde4982e080c5f6836060f1
Marta Hilgert <marta.hil.1988@gmail.com>
2024-12-08 20:38:44 -0300

    Home: inputs enhanced

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -7,16 +7,27 @@ export function Home() {
       <form>
         <FormContainer>
           <label htmlFor="task">I will work on</label>
-          <TaskInput 
-            id="task" 
-            placeholder="Give your project a name" 
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Give your project a name"
           />
 
+          <datalist id="task-suggestions">
+            <option value="Project 1" />
+            <option value="Project 2" />
+            <option value="Project 3" />
+            <option value="Banana" />
+          </datalist>
+
           <label htmlFor="">during</label>
-          <MinutesAmountInput 
-            type="number" 
-            id="minutesAmount"  
-            placeholder="00" 
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutes.</span>
