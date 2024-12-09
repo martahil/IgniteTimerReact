cfced76818e7d581cfe21d324841fc633db1905d
Marta Hilgert <marta.hil.1988@gmail.com>
2024-12-09 20:02:57 -0300

    Use react-hook-form to manage form inputs

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,16 +1,27 @@
 import { Play } from "phosphor-react";
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
+import { useForm } from "react-hook-form";
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm()
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task;
+
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">I will work on</label>
           <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="Give your project a name"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -28,6 +39,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutes.</span>
@@ -41,9 +53,9 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
-          Start
+         <StartCountdownButton disabled={isSubmitDisabled} type="submit"> 
           <Play size={24} />
+          Start
         </StartCountdownButton>
       </form>
     </HomeContainer>
