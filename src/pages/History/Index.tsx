f4554c2eb50f9ef29a48037d270656efb047ccd7
Marta Hilgert <marta.hil.1988@gmail.com>
2024-12-09 18:59:42 -0300

    Implement task status features in History

--- src/pages/History/Index.tsx
+++ src/pages/History/Index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
   return (
@@ -20,37 +20,57 @@ export function History() {
               <td>Read "A Song of Ice and Fire"</td>
               <td>20 minutes</td>
               <td>2 months ago</td>
-              <td>Completed</td>
+              <td>
+                <Status statusColor="green">Completed</Status>
+              </td>              
             </tr>
             <tr>
               <td>Work on Project C</td>
               <td>50 minutes</td>
               <td>1 month ago</td>
-              <td>Completed</td>
+              <td>
+                <Status statusColor="yellow">In Progress</Status>
+              </td>      
             </tr>
             <tr>
               <td>Get a quote for a new air conditioner for the house</td>
               <td>30 minutes</td>
               <td>1 month ago</td>
-              <td>Completed</td>
+              <td>
+                <Status statusColor="red">Interrupted</Status>
+              </td>      
             </tr>
             <tr>
               <td>Clean the kitchen</td>
               <td>40 minutes</td>
               <td>2 month ago</td>
-              <td>Completed</td>
+              <td>
+                <Status statusColor="green">Completed</Status>
+              </td>      
             </tr>
             <tr>
               <td>Play with the dog</td>
               <td>20 minutes</td>
               <td>3 month ago</td>
-              <td>Completed</td>
+              <td>
+                <Status statusColor="green">Completed</Status>
+              </td>      
             </tr>
             <tr>
               <td>Write the script for project F</td>
               <td>30 minutes</td>
               <td>1 month ago</td>
-              <td>Completed</td>
+              <td>
+                <Status statusColor="green">Completed</Status>
+              </td>      
+            </tr>
+            <tr>
+              <td>Play with the cat</td>
+              <td>10 minutes</td>
+              <td>2 month ago</td>
+              <td>
+                <Status statusColor="green">Completed</Status>
+              </td>      
             </tr>
           </tbody>
         </table>
