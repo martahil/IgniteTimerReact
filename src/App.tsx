7855de7186ec101364d0cca5c28a95e2202340a9
Marta Hilgert <marta.hil.1988@gmail.com>
2025-01-07 20:00:17 -0300

    Enable access to Context data on the History page

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import { Router } from './Router';
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/themes/global'
+import { CyclesContextProvider } from './contexts/CyclesContext';
 
-export function App() {
+export function App() { 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
+      <BrowserRouter>    
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>  
       </BrowserRouter>
 
       <GlobalStyle />
