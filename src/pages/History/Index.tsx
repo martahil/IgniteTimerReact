6342f513951c5bd892bf673af6f4e3ef18bda79b
Marta Hilgert <marta.hil.1988@gmail.com>
2025-01-07 20:50:00 -0300

    Display task list with statuses (completed, interrupted, and in progress) on the History page

--- src/pages/History/Index.tsx
+++ src/pages/History/Index.tsx
@@ -9,10 +9,6 @@ export function History() {
     <HistoryContainer>
       <h1>My history</h1>
 
-      <pre>
-        {JSON.stringify(cycles, null, 2)}
-      </pre>
-
       <HistoryList>
         <table>
           <thead>
@@ -24,62 +20,28 @@ export function History() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Read "A Song of Ice and Fire"</td>
-              <td>20 minutes</td>
-              <td>2 months ago</td>
-              <td>
-                <Status statusColor="green">Completed</Status>
-              </td>              
-            </tr>
-            <tr>
-              <td>Work on Project C</td>
-              <td>50 minutes</td>
-              <td>1 month ago</td>
-              <td>
-                <Status statusColor="yellow">In Progress</Status>
-              </td>      
-            </tr>
-            <tr>
-              <td>Get a quote for a new air conditioner for the house</td>
-              <td>30 minutes</td>
-              <td>1 month ago</td>
-              <td>
-                <Status statusColor="red">Interrupted</Status>
-              </td>      
-            </tr>
-            <tr>
-              <td>Clean the kitchen</td>
-              <td>40 minutes</td>
-              <td>2 month ago</td>
-              <td>
-                <Status statusColor="green">Completed</Status>
-              </td>      
-            </tr>
-            <tr>
-              <td>Play with the dog</td>
-              <td>20 minutes</td>
-              <td>3 month ago</td>
-              <td>
-                <Status statusColor="green">Completed</Status>
-              </td>      
-            </tr>
-            <tr>
-              <td>Write the script for project F</td>
-              <td>30 minutes</td>
-              <td>1 month ago</td>
-              <td>
-                <Status statusColor="green">Completed</Status>
-              </td>      
-            </tr>
-            <tr>
-              <td>Play with the cat</td>
-              <td>10 minutes</td>
-              <td>2 month ago</td>
-              <td>
-                <Status statusColor="green">Completed</Status>
-              </td>      
-            </tr>
+            {cycles.map(cycle => {
+              return (
+                <tr key={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount} minutes</td>
+                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>
+                    {cycle.finishedDate && (
+                      <Status statusColor="green">Completed</Status>
+                    )}
+
+                    {cycle.interruptedDate && (
+                      <Status statusColor="red">Interrupted</Status>
+                    )}
+
+                    {(!cycle.finishedDate && !cycle.interruptedDate) && (
+                      <Status statusColor="yellow">In progress</Status>
+                    )}
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
       </HistoryList>
